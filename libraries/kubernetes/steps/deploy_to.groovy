421d900732e0f46b4e1b1f4177ea317935f7ab07
Steven Terrana <steventerrana@gmail.com>
2021-02-19 19:52:45 -0500

    update

--- libraries/kubernetes/steps/deploy_to.groovy
+++ libraries/kubernetes/steps/deploy_to.groovy
@@ -110,8 +110,8 @@ void update_values_file(String values_file, String config_repo){
   values = readYaml file: values_file
   println "git URL var is: ${env.GIT_URL}"
   values.find{ k, v -> 
-      if(v.getValue().containsKey("github_repo")){
-        v.getValue().github_repo.equals(env.GIT_URL)
+      if(v.github_repo){
+        v.github_repo.equals(env.GIT_URL)
       }
   }.getValue().image.tag = env.GIT_SHA 
   sh "rm ${values_file}"
